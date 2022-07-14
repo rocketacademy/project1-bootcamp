7c07673d79961b2e73e48c659ac628d433f8735a
Erilcartul <Fabiankhoo517@gmail.com>
2022-07-14 22:28:07 +0800

    NEW

---
+++ server/server.js
@@ -0,0 +1,57 @@
+const express = require("express");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const SpotifyWebApi = require("spotify-web-api-node");
+
+const app = express();
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.post("/refresh", (req, res) => {
+  const refreshToken = req.body.refreshToken;
+  const spotifyApi = new SpotifyWebApi({
+    redirectUri: "http://localhost:3000",
+    clientId: "a2be003ef624499594b9bb757c703dd5",
+    clientSecret: "06245893f38c4fbaa07829f53a21d3ac",
+    refreshToken,
+  });
+
+  spotifyApi
+    .refreshAccessToken()
+    .then((data) => {
+      res.json({
+        accessToken: data.body.access_token,
+        expiresIn: data.body.expires_in,
+      });
+    })
+    .catch((err) => {
+      console.log(err);
+      res.sendStatus(400);
+    });
+});
+
+app.post("/login", (req, res) => {
+  const code = req.body.code;
+  const spotifyApi = new SpotifyWebApi({
+    redirectUri: "http://localhost:3000",
+    clientId: "a2be003ef624499594b9bb757c703dd5",
+    clientSecret: "06245893f38c4fbaa07829f53a21d3ac",
+  });
+
+  spotifyApi
+    .authorizationCodeGrant(code)
+    .then((data) => {
+      res.json({
+        accessToken: data.body.access_token,
+        refreshToken: data.body.refresh_token,
+        expiresIn: data.body.expires_in,
+      });
+      console.log(data);
+    })
+    .catch((err) => {
+      res.sendStatus(400);
+    });
+});
+
+app.listen(3001);
