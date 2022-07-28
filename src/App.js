383df406ebf7b8988f207c69be4a14d6a30e232e
KATSUSANDWICH <MEOWSPOKENHERE@GMAIL.COM>
2022-07-28 17:30:15 +0800

    tentative skeleton code for input boxes and table

--- src/App.js
+++ src/App.js
@@ -2,15 +2,54 @@ import React from "react";
 import logo from "./logo.png";
 import "./App.css";
 
+// import Table from "@mui/material/Table";
+// import TableBody from "@mui/material/TableBody";
+// import TableCell from "@mui/material/TableCell";
+// import TableContainer from "@mui/material/TableContainer";
+// import TableHead from "@mui/material/TableHead";
+// import TableRow from "@mui/material/TableRow";
+// import Paper from "@mui/material/Paper";
+
 class App extends React.Component {
+  //constructor
+  constructor(props) {
+    super(props);
+    this.state = {
+      datetime: "",
+      tasks: [],
+    };
+  }
+
+  //handleChange
+
+  //handleSubmit which will move input into the table
+
   render() {
+    // function that will map the input into table modelled on the following sample
+    // let messageListItems = this.state.messages.map((message) => (
+    //   <li key={message.key}>{message.val}</li>
+    // ));
+
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
+
+          {/* <div>{TableContainer}</div> */}
+          <form>
+            <div>
+              <p>
+                <label>Date and Time: </label>
+                <input type="text" value={new Date().toLocaleString() + ""} />
+              </p>
+              <p>
+                <label>Task: </label>
+                <input type="text" />
+                <input type="submit" value="Submit" />
+              </p>
+            </div>
+          </form>
+          {/* call function here <ol>{messageListItems}</ol> */}
         </header>
       </div>
     );
