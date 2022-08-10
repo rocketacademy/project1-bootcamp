d67c37dee4e829090ad8d1419fa6f6be94a68a8c
KATSUSANDWICH <MEOWSPOKENHERE@GMAIL.COM>
2022-08-10 21:29:13 +0100

    end of 10 Aug - not showing task but showing date

--- src/App.js
+++ src/App.js
@@ -30,7 +30,7 @@ const App = () => {
   const [completedTasks, setCompletedTasks] = useState([]);
 
   const updateCompletedTasks = (currTask) => {
-    setCompletedTasks((prev) => [...prev, { name: currTask }]);
+    setCompletedTasks((prev) => [...prev, { text: task }]);
   };
 
   // // const [secondsBalance, setSecondsBalance] = useState([100]);
@@ -109,7 +109,6 @@ const App = () => {
           />
           <input type="submit" value="Submit" />
         </form>
-        {/* <li>{JSON.stringify(allTasks)}</li> */}
         <tr>
           <th>Date and Time </th>
           <th>Task </th>
@@ -132,85 +131,3 @@ const App = () => {
 };
 
 export default App;
-
-// const CountdownApp = ({
-//   secondsRemaining,
-//   setSecondsRemaining,
-//   INITIAL_COUNT,
-// }) => {
-//   // source: https://overreacted.io/making-setinterval-declarative-with-react-hooks/
-//   const useInterval = (callback, delay) => {
-//     const savedCallback = useRef();
-//     // Remember the latest callback.
-//     useEffect(() => {
-//       savedCallback.current = callback;
-//     }, [callback]);
-//     // Set up the interval.
-//     useEffect(() => {
-//       function tick() {
-//         savedCallback.current();
-//       }
-//       if (delay !== null) {
-//         let id = setInterval(tick, delay);
-//         return () => clearInterval(id);
-//       }
-//     }, [delay]);
-//   };
-//   // https://stackoverflow.com/a/2998874/1673761
-//   const twoDigits = (num) => String(num).padStart(2, "0");
-//   const STATUS = {
-//     STARTED: "Started",
-//     STOPPED: "Stopped",
-//   };
-//   // const INITIAL_COUNT = 120;
-//   // const [secondsRemaining, setSecondsRemaining] = useState(INITIAL_COUNT);
-//   const [status, setStatus] = useState(STATUS.STOPPED);
-//   const secondsToDisplay = secondsRemaining % 60;
-//   const minutesRemaining = (secondsRemaining - secondsToDisplay) / 60;
-//   const minutesToDisplay = minutesRemaining % 60;
-//   const hoursToDisplay = (minutesRemaining - minutesToDisplay) / 60;
-//   const handleStart = () => {
-//     setStatus(STATUS.STARTED);
-//   };
-//   const handleStop = () => {
-//     setStatus(STATUS.STOPPED);
-//   };
-//   const handleReset = () => {
-//     setStatus(STATUS.STOPPED);
-//     setSecondsRemaining(INITIAL_COUNT);
-//   };
-//   useInterval(
-//     () => {
-//       if (secondsRemaining > 0) {
-//         setSecondsRemaining(secondsRemaining - 1);
-//       } else {
-//         setStatus(STATUS.STOPPED);
-//       }
-//     },
-//     status === STATUS.STARTED ? 1000 : null
-//     // passing null stops the interval
-//   );
-//   return (
-//     <div className="App">
-//       {/* <h1>React Countdown Demo</h1> */}
-//       <button onClick={handleStart} type="button">
-//         Start
-//       </button>
-//       <button onClick={handleStop} type="button">
-//         Stop
-//       </button>
-//       <button onClick={handleReset} type="button">
-//         Reset
-//       </button>
-//       <div
-//         style={{
-//           padding: 20,
-//         }}
-//       >
-//         {twoDigits(hoursToDisplay)}:{twoDigits(minutesToDisplay)}:
-//         {twoDigits(secondsToDisplay)}
-//       </div>
-//       <div>Status: {status}</div>
-//     </div>
-//   );
-// }
