b1984921a7dbe9a7808f32eadf63b8f86cad6824
KATSUSANDWICH <MEOWSPOKENHERE@GMAIL.COM>
2022-08-11 11:34:05 +0100

    shortened version which does not render task name

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { useState, useEffect, useRef } from "react";
-import ReactDOM from "react-dom/client";
 import logo from "./logo.png";
 import CountdownTimer from "./CountdownTimer";
 import "./App.css";
@@ -7,25 +6,22 @@ import "./App.css";
 const App = () => {
   //task related drafting
   const [task, setTask] = useState({
-    currDateEntry: new Date().toLocaleString() + "",
+    id: new Date().toLocaleString() + "",
     text: "",
   });
   //allTasks, setAllTasks
   const [allTasks, setAllTasks] = useState([]);
   // use handleChange
   const handleChange = (event) => {
-    // setInputValue(event.target.value);
     setTask((prev) => ({ ...prev, text: event.target.value }));
   };
   //usehandleSubmit
   const handleSubmit = (event) => {
     event.preventDefault();
-    // setAllTasks((prev) => [...prev, currDateEntry + " " + inputValue]);
     setAllTasks((prev) => [...prev, task]);
-    // console.log(`inputValue is ${inputValue}, and alltasks is ${allTasks}.`);
-    // setInputValue("");
-    setTask({ currDateEntry: new Date().toLocaleString() + "", text: "" });
+    setTask({ id: new Date().toLocaleString() + "", text: "" });
   };
+
   //completedTasks, setCompletedTasks - setCompletedTasks is triggered when the timer runs out
   const [completedTasks, setCompletedTasks] = useState([]);
 
@@ -33,66 +29,24 @@ const App = () => {
     setCompletedTasks((prev) => [...prev, { text: task }]);
   };
 
-  // // const [secondsBalance, setSecondsBalance] = useState([100]);
-  // const [isTaskCompleted, setIsTaskCompleted] = useState(false);
-  // //task toggler
-  // var toggler = () => {
-  //   if (secondsRemaining === 0) {
-  //     setIsTaskCompleted((isTaskCompleted) => !isTaskCompleted);
-  //   }
-  //   if (isTaskCompleted === true) {
-  //     var shifted = allTasks.shift();
-  //     setCompletedTasks((prev) => [...prev, shifted]);
-  //   }
-  // };
   //massage the tasksLIst rendering function
   let taskItems = allTasks.map((allTasks) => (
     <table>
-      {/* <tr>
-        <th>Date and Time</th>
-        <th>Task</th>
-        <th>Timer</th>
-      </tr> */}
       <tr>
-        <td>{allTasks.currDateEntry}</td>
+        <td>{allTasks.id}</td>
         <td>{allTasks.text}</td>
         <td>
           <div>
-            <CountdownTimer
-              updateCompletedTasks={updateCompletedTasks}
-              // secondsRemaining={secondsRemaining}
-              // setSecondsRemaining={setSecondsRemaining}
-            />
+            <CountdownTimer updateCompletedTasks={updateCompletedTasks} />
           </div>
-          {/* {secondsBalance} */}
-          {/* <input type="submit" value="Timer start" onClick={timer} /> */}
-          {/* <input
-            type="submit"
-            value="Timer placeholder"
-            onClick={toggleTester}/> */}
-          {/* <input type="submit" value="Timer stop" onClick={useEffect()} /> */}
         </td>
       </tr>
     </table>
   ));
   // massage the completed tasks rendering function
+
   let completedTaskItems = JSON.stringify(completedTasks);
 
-  // let completedTaskItems = completedTasks.map((completedTasks) => (
-  //   <table>
-  //     {/* <tr>
-  //       <th>Date and Time</th>
-  //       <th>Task</th>
-  //       <th>Timer</th>
-  //     </tr> */}
-  //     <tr>
-  //       <td>{completedTasks.currDateEntry}</td>
-  //       <td>{completedTasks.text}</td>
-  //       <td>Number of minutes placeholder</td>
-  //     </tr>
-  //   </table>
-  // ));
-  // return render function
   return (
     <div className="App">
       <header className="App-header">
@@ -103,7 +57,6 @@ const App = () => {
         <form onSubmit={handleSubmit}>
           <input
             placeholder="New task"
-            // value={inputValue}
             value={task.text}
             onChange={handleChange}
           />
