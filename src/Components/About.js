223be3605c5478cf69287f37f762e88424e8bc6d
LoyChaiEe <celoy001@gmail.com>
2022-10-15 11:27:36 +0800

    initiate about component

--- src/Components/About.js
+++ src/Components/About.js
@@ -1,29 +1,44 @@
 import React from "react";
-import { Container } from "react-bootstrap";
+import { Container, Row, Col } from "react-bootstrap";
 
 class About extends React.Component {
   render() {
     return (
-      <div>
-        <h2>About</h2>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-          aliquip ex ea commodo consequat. Duis aute irure dolor in
-          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-          culpa qui officia deserunt mollit anim id est laborum.
-        </p>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-          aliquip ex ea commodo consequat. Duis aute irure dolor in
-          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-          culpa qui officia deserunt mollit anim id est laborum.
-        </p>
+      <div id="about">
+        <Row>
+          <Col>
+            <h2>About me</h2>
+            <p>
+              Hello! I'm Chai Ee. I'm usually that chill down-to-earth guy and
+              once you get to know me, you will find that I'm an extrovert who
+              communicates well and builds positive relationships with people I
+              work with. I am a creative and quick-thinking individual who is
+              always game for a challenge. I like coding, especially frontend,
+              and am passionate about designing eye-catching and secured
+              websites. Fresh out of university, I'm looking for exciting
+              opportunities to jumpstart my career journey in the IT and Cyber
+              Security field.
+            </p>
+          </Col>
+          <Col>
+            <Row>
+              <h2>Bio</h2>
+              <Col>
+                <div>age</div>
+                <div>email</div>
+                <div>phone</div>
+                <div>Address</div>
+              </Col>
+              <Col>
+                <div>25</div>
+                <div>chaiee@live.com.sg</div>
+                <div>+65 92972517</div>
+                <div>Singapore, Singapore</div>
+              </Col>
+            </Row>
+            <div>icons icons icons icons</div>
+          </Col>
+        </Row>
       </div>
     );
   }
--- src/Components/Project.js
+++ src/Components/Project.js
@@ -1,33 +1,74 @@
-import React from "react";
+import React, { useState } from "react";
+import Carousel from "react-bootstrap/Carousel";
+import logo from "../logo.png"
+import Button from "react-bootstrap/Button";
+import Card from "react-bootstrap/Card";
 
-class Project extends React.Component{
-  
+function Project() {
+  const [index, setIndex] = useState(0);
 
-  render(){
-    return (
-      <div id="project">
-        <h2>Project</h2>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-          aliquip ex ea commodo consequat. Duis aute irure dolor in
-          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-          culpa qui officia deserunt mollit anim id est laborum.
-        </p>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-          aliquip ex ea commodo consequat. Duis aute irure dolor in
-          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-          culpa qui officia deserunt mollit anim id est laborum.
-        </p>
-      </div>
-    );
-  }
+  const handleSelect = (selectedIndex, e) => {
+    setIndex(selectedIndex);
+  };
+
+  return (
+    <Carousel activeIndex={index} onSelect={handleSelect}>
+      <Carousel.Item
+        style={{
+          display: "flex",
+          justifyContent: "center",
+        }}
+      >
+        <Card style={{ width: "18rem" }}>
+          <Card.Img variant="top" src={logo} />
+          <Card.Body>
+            <Card.Title>Card Title 1</Card.Title>
+            <Card.Text>
+              Some quick example text to build on the card title and make up the
+              bulk of the card's content.
+            </Card.Text>
+            <Button variant="primary">See website</Button>
+          </Card.Body>
+        </Card>
+      </Carousel.Item>
+      <Carousel.Item
+        style={{
+          display: "flex",
+          justifyContent: "center",
+        }}
+      >
+        <Card style={{ width: "18rem" }}>
+          <Card.Img variant="top" src={logo} />
+          <Card.Body>
+            <Card.Title>Card Title 2</Card.Title>
+            <Card.Text>
+              Some quick example text to build on the card title and make up the
+              bulk of the card's content.
+            </Card.Text>
+            <Button variant="primary">Go somewhere</Button>
+          </Card.Body>
+        </Card>
+      </Carousel.Item>
+      <Carousel.Item
+        style={{
+          display: "flex",
+          justifyContent: "center",
+        }}
+      >
+        <Card style={{ width: "18rem" }}>
+          <Card.Img variant="top" src={logo} />
+          <Card.Body>
+            <Card.Title>Card Title 3</Card.Title>
+            <Card.Text>
+              Some quick example text to build on the card title and make up the
+              bulk of the card's content.
+            </Card.Text>
+            <Button variant="primary">Go somewhere</Button>
+          </Card.Body>
+        </Card>
+      </Carousel.Item>
+    </Carousel>
+  );
 }
 
-export default Project
+export default Project;
