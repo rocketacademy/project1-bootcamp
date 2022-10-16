4237bd4c1b7f0a867dfe19f633aa6a7e12600d9d
oheirelavv <oheirelav@gmail.com>
2022-10-16 22:55:39 +0800

    updated App.js

--- src/App.js
+++ src/App.js
@@ -1,17 +1,51 @@
 import React from "react";
-import logo from "./logo.png";
+// import logo from "./logo.png";
 import "./App.css";
+import {Board} from "./components/Board"
+import {Timer} from "./components/Timer"
+import {Settings} from "./components/Settings/Settings"
+import {RestartGame} from "./components/RestartGame"
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state= {
+      gameOptions: {}
+    }
+    
+  }
+
+  startGame() {
+    this.setState(() => ({ 
+      gameOptions: this.startGame}))
+  }
+  
+  // resetGame() {
+  //   this.setState({
+  //     gameOptions: null
+  //   })
+  // }
+
   render() {
+    console.log(this.state)
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-        </header>
+        {/* <header className="App-header"> */}
+          <div className="header">Match the words!</div>
+          {!this.gameOptions ? (
+            <Settings startGame={this.startGame} />
+          ) : (
+          <div>
+          <Timer />
+          <Board />
+          </div>)
+          }
+          {/* <Settings startGame= {() => this.startGame}  /> */}
+          
+          <Board />
+          <RestartGame resetGame={this.resetGame} />
+          
+        {/* </header> */}
       </div>
     );
   }
