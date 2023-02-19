0387e52038ec4b4c3a526c7a61342a359f38d688
peanutyabing <sherry.liuyabing@gmail.com>
2023-02-19 17:46:48 +0800

    Refactor waffler-finder and improve word finding logic to introduce more randomness

--- src/Waffle-maker/waffle-finder.js
+++ src/Waffle-maker/waffle-finder.js
@@ -20,23 +20,10 @@ function findWaffle(row0word) {
     col0word = getRandomWord([...indexedWords[row0word[0]]]);
     col2word = getRandomWord([...indexedWords[row0word[2]]]);
     col4word = getRandomWord([...indexedWords[row0word[4]]]);
-
-    for (const word of indexedWords[col0word[2]]) {
-      if (word[2] === col2word[2] && word[4] === col4word[2]) {
-        row2word = word;
-      } else {
-        row2word = "";
-      }
-    }
-
-    for (const word of indexedWords[col0word[4]]) {
-      if (word[2] === col2word[4] && word[4] === col4word[4]) {
-        row4word = word;
-      } else {
-        row4word = "";
-      }
-    }
+    row2word = findMatchingWord(col0word[2], col2word[2], col4word[2]);
+    row4word = findMatchingWord(col0word[4], col2word[4], col4word[4]);
   }
+
   [row0word, row2word, row4word, col0word, col2word, col4word] = [
     row0word,
     row2word,
@@ -46,4 +33,18 @@ function findWaffle(row0word) {
     col4word,
   ].map((word) => word.toUpperCase());
   return `${row0word}${col0word[1]} ${col2word[1]} ${col4word[1]}${row2word}${col0word[3]} ${col2word[3]} ${col4word[3]}${row4word}`;
+}
+
+function findMatchingWord(firstLetter, middleLetter, lastLetter) {
+  let match = "";
+  const matches = [];
+  for (const word of indexedWords[firstLetter]) {
+    if (word[2] === middleLetter && word[4] === lastLetter) {
+      matches.push(word);
+    }
+  }
+  if (matches.length) {
+    match = getRandomWord(matches);
+  }
+  return match;
 }
