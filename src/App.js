a2faab4e3c7bd08800b023ad5963752febfcc8e9
peanutyabing <sherry.liuyabing@gmail.com>
2023-02-19 22:36:05 +0800

    Render game rules modal

--- src/App.js
+++ src/App.js
@@ -1,18 +1,25 @@
 import React from "react";
 import "./App.css";
 import Tiles from "./Components/Tiles";
+import Button from "react-bootstrap/Button";
+import RulesModal from "./Components/Modal";
 
-class App extends React.Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <h1>WAFFLE</h1>
-          <Tiles />
-        </header>
-      </div>
-    );
-  }
+function App() {
+  const [modalShow, setModalShow] = React.useState(false);
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Button variant="dark" onClick={() => setModalShow(true)}>
+          ❔
+        </Button>
+        <h1>WAFFLE</h1>
+        <Tiles />
+      </header>
+
+      <RulesModal show={modalShow} onHide={() => setModalShow(false)} />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/Components/Modal.js
@@ -0,0 +1,79 @@
+import React from "react";
+import Button from "react-bootstrap/Button";
+import Modal from "react-bootstrap/Modal";
+
+export default class RulesModal extends React.Component {
+  render() {
+    return (
+      <Modal
+        {...this.props}
+        size="lg"
+        aria-labelledby="contained-modal-title-vcenter"
+        centered
+      >
+        <Modal.Header closeButton>
+          <Modal.Title id="contained-modal-title-vcenter">
+            HOW TO PLAY
+          </Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <p>Solve the WAFFLE in as few moves as possible to earn toppings.</p>
+          <p>
+            Rearrange the letters into the correct words, horizontally or
+            vertically. Click letters to swop their positions anywhere on the
+            board. The letters will change color to show whether they are in the
+            correct position. The number of moves remaining is displayed below
+            the board.
+          </p>
+          <p>
+            In this example, the "S"es are in the correct place. The "A" is in
+            the word, but in a different position. <br />
+            <img
+              src={require("./images/waffle-row-example.png")}
+              alt=""
+              style={{ maxWidth: "70%" }}
+            />
+          </p>
+          <p>
+            When the yellow letter is on a corner, either the vertical word or
+            the horizontal word contains this letter.
+            <br />
+            <img
+              src={require("./images/waffle-junction-example.png")}
+              alt=""
+              style={{ maxWidth: "42%" }}
+            />
+          </p>{" "}
+          <br />
+          <h4>SPECIAL CASES</h4>
+          <h6>THE SAME LETTER MULTIPLE TIMES IN ONE WORD</h6>
+          <p>
+            In this case, the word must contain only one "L", as the first is
+            yellow but the second is grey. Neither of them are in the correct
+            position. <br />
+            <img
+              src={require("./images/waffle-oney-onegy-example.png")}
+              alt=""
+              style={{ maxWidth: "70%" }}
+            />
+          </p>
+          <p>
+            Once we have an "L" in the correct place, the first "L" is no longer
+            needed in the word, so, it is no longer yellow. <br />
+            <img
+              src={require("./images/waffle-onegn-onegy-example.png")}
+              alt=""
+              style={{ maxWidth: "70%" }}
+            />
+          </p>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button onClick={this.props.onHide}>Close</Button>
+          Built by <a href="https://github.com/peanutyabing">peanutyabing</a> |
+          Created by <a href="https://twitter.com/jamesjessian">jessian</a> |
+          Original game at <a href="https://wafflegame.net/">wafflegame.net</a>
+        </Modal.Footer>
+      </Modal>
+    );
+  }
+}
