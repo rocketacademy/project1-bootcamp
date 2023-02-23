1056477fcdc5c3d8c43d515906cc4a2b42c8d6e6
peanutyabing <sherry.liuyabing@gmail.com>
2023-02-23 09:55:31 +0800

    Final touchup

--- src/Components/Offcanvas.js
+++ src/Components/Offcanvas.js
@@ -2,27 +2,21 @@ import React from "react";
 import Offcanvas from "react-bootstrap/Offcanvas";
 
 export default class Menu extends React.Component {
-  // handleClick = (e) => {
-
-  // };
-
   render() {
     return (
-      <>
-        <Offcanvas {...this.props}>
-          <Offcanvas.Header closeButton closeVariant="white">
-            <Offcanvas.Title>MENU</Offcanvas.Title>
-          </Offcanvas.Header>
-          <Offcanvas.Body>
-            <div className="menu-selection" onClick={this.props.onClick}>
-              DAILY WAFFLE
-            </div>
-            <div className="menu-selection" onClick={this.props.onClick}>
-              WAFFLE BUFFET
-            </div>
-          </Offcanvas.Body>
-        </Offcanvas>
-      </>
+      <Offcanvas {...this.props}>
+        <Offcanvas.Header closeButton closeVariant="white">
+          <Offcanvas.Title>MENU</Offcanvas.Title>
+        </Offcanvas.Header>
+        <Offcanvas.Body>
+          <div className="menu-selection" onClick={this.props.onClick}>
+            DAILY WAFFLE
+          </div>
+          <div className="menu-selection" onClick={this.props.onClick}>
+            WAFFLE BUFFET
+          </div>
+        </Offcanvas.Body>
+      </Offcanvas>
     );
   }
 }
