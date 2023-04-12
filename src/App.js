f512bbcdbced9b5218ec377b2ebc2d3c195f09f7
prettykaur <pkaur.dev@gmail.com>
2023-04-12 18:30:41 +0800

    Import Game.js

--- src/App.js
+++ src/App.js
@@ -1,17 +1,12 @@
 import React from "react";
-import logo from "./logo.png";
 import "./App.css";
+import Game from "./Components/Game";
 
 class App extends React.Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-        </header>
+        <Game />
       </div>
     );
   }
