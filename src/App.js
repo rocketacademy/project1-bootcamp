89b4d348be0ce3fe1450cdb43daab1fada541b94
alaynelxr <alaynelxr@gmail.com>
2023-04-29 16:15:19 +0800

    added checkbox functionality with states

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import logo from "./assets/logo.png";
 import "./App.css";
+import { IngredientList } from "./pages/Ingredients/ingredientsPage";
 
 class App extends React.Component {
   render() {
@@ -8,9 +9,7 @@ class App extends React.Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
+          <IngredientList />
         </header>
       </div>
     );
