50992b0ad6c2cce78fca373796dc56df09f88b4d
dexterch91 <dexterchewxh@hotmail.sg>
2023-04-30 18:23:23 +0800

    added props input into stopwatch, added circular progress bar component, added image input

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ class App extends React.Component {
     return (
       <div className="App">
         <header className="App-header">
-          <StopWatch />
+          <StopWatch setTime={3}/>
         </header>
       </div>
     );
