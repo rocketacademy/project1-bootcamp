4d29a105f5ab288a5abbd9024257d0586fb70a8e
dexterch91 <dexterchewxh@hotmail.sg>
2023-05-01 15:26:26 +0800

    altered to use react class components instead

--- src/components/Stopwatch.js
+++ src/components/Stopwatch.js
@@ -1,35 +1,42 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 
-function StopWatch() {
-  const [time, setTime] = useState(300); // initial time in seconds (5 minutes)
-  const [isActive, setIsActive] = useState(false);
+class Stopwatch extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      time: 300, // initial time in seconds (5 minutes)
+      isActive: false,
+    };
+    this.intervalId = null;
+  }
 
-  useEffect(() => {
-    let intervalId;
+  componentDidMount() {
+    this.intervalId = setInterval(() => {
+      if (this.state.isActive && this.state.time > 0) {
+        this.setState((prevState) => ({
+          time: prevState.time - 1,
+        }));
+      }
+    }, 1000);
+  }
 
-    if (isActive && time > 0) {
-      intervalId = setInterval(() => {
-        setTime(time - 1); //count down per second
-      }, 1000);
-    }
+  componentWillUnmount() {
+    clearInterval(this.intervalId);
+  }
 
-    return () => clearInterval(intervalId);
-  }, [isActive, time]);
-
-  const startTimer = () => {
-    setIsActive(true);
+  startTimer = () => {
+    this.setState({ isActive: true });
   };
 
-  const stopTimer = () => {
-    setIsActive(false);
+  stopTimer = () => {
+    this.setState({ isActive: false });
   };
 
-  const resetTimer = () => {
-    setTime(300);
-    setIsActive(false);
+  resetTimer = () => {
+    this.setState({ time: 300, isActive: false });
   };
 
-  const formatTime = (seconds) => {
+  formatTime = (seconds) => {
     const minutes = Math.floor(seconds / 60);
     const remainingSeconds = seconds % 60;
     const formattedMinutes = String(minutes).padStart(2, "0");
@@ -37,14 +44,16 @@ function StopWatch() {
     return `${formattedMinutes}:${formattedSeconds}`;
   };
 
-  return (
-    <div>
-      <div>{formatTime(time)}</div>
-      <button onClick={startTimer}>Start</button>
-      <button onClick={stopTimer}>Stop</button>
-      <button onClick={resetTimer}>Reset</button>
-    </div>
-  );
+  render() {
+    return (
+      <div>
+        <div>{this.formatTime(this.state.time)}</div>
+        <button onClick={this.startTimer}>Start</button>
+        <button onClick={this.stopTimer}>Stop</button>
+        <button onClick={this.resetTimer}>Reset</button>
+      </div>
+    );
+  }
 }
 
-export default StopWatch;
+export default Stopwatch;
