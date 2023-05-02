acec2456ba9d42223ca403ca93aef887e115b76d
root <root@DESKTOP-9SFSVP3>
2023-05-02 20:12:16 +0800

    added homepage

--- src/App.js
+++ src/App.js
@@ -1,17 +1,16 @@
 import React from "react";
-import logo from "./assets/logo.png";
 import "./App.css";
+import HomePage from "./pages/Homepage/homepage";
 
 class App extends React.Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-        </header>
+        <HomePage 
+          backgroundImage="https://img.us.news.samsung.com/us/wp-content/uploads/2018/01/14112220/180108_FH_AKG-Speaker_Full-Shot_w_homescreen_rgb_04.jpg"
+          title="What's left in your fridge today?"
+          buttonText="Let's take a look!"
+        />
       </div>
     );
   }
