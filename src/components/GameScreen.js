631af04aa35675a226b0ccfe5e21f49b74e6de5e
chengwaikoo <chengwai.koo@gmail.com>
2023-07-30 06:08:29 +0800

    refactor: move map into its own component

--- src/components/GameScreen.js
+++ src/components/GameScreen.js
@@ -1,23 +1,8 @@
 import React, { useRef, useEffect } from "react";
-import maplibregl from "maplibre-gl";
+import Map from "./Map.js";
 
 function GameScreen() {
-  const mapContainer = useRef(null);
-
-  useEffect(() => {
-    const map = new maplibregl.Map({
-      container: mapContainer.current,
-      style: `https://api.maptiler.com/maps/basic-v2/style.json?key=${process.env.REACT_APP_MAPTILER_KEY}`,
-      center: [103.8198, 1.3521],
-      zoom: 10,
-    });
-
-    return () => map.remove();
-  }, []);
-
-  return (
-    <div ref={mapContainer} style={{ width: "100%", height: "100vh" }}></div>
-  );
+  return <Map />;
 }
 
 export default GameScreen;
---
+++ src/components/Map.js
@@ -0,0 +1,23 @@
+import React, { useRef, useEffect } from "react";
+import maplibregl from "maplibre-gl";
+
+function Map() {
+  const mapContainer = useRef(null);
+
+  useEffect(() => {
+    const map = new maplibregl.Map({
+      container: mapContainer.current,
+      style: `https://api.maptiler.com/maps/basic-v2/style.json?key=${process.env.REACT_APP_MAPTILER_KEY}`,
+      center: [103.8198, 1.3521],
+      zoom: 10,
+    });
+
+    return () => map.remove();
+  }, []);
+
+  return (
+    <div ref={mapContainer} style={{ width: "100%", height: "100vh" }}></div>
+  );
+}
+
+export default Map;
