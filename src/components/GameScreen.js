7df9075c1da74db89874fa8d8f4a1e8f455693f3
chengwaikoo <chengwai.koo@gmail.com>
2023-07-30 10:18:45 +0800

    feat: base height on 100% not 100vh

--- src/components/GameScreen.js
+++ src/components/GameScreen.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import {
-  Container,
+  // Container,
   Flex,
   Paper,
   Button,
@@ -35,14 +35,14 @@ function GameScreen() {
     <Flex
       w={`calc(100% - 2 * ${theme.spacing.lg})`}
       maw="900px"
-      h={`calc(100vh - 2 * ${theme.spacing.lg})`}
+      h="100%"
       gap="lg"
       direction="column"
       m="lg"
     >
       <Paper px="md">
         <Text align="center" py="sm">
-          {placeName} where ah?
+          <span id="place-name">{placeName}</span> where ah?
         </Text>
       </Paper>
 
@@ -52,11 +52,12 @@ function GameScreen() {
 
       <Paper className={classes.bottomPaper}>
         <Flex w="100%" align="center" justify="space-between" px="md" py="sm">
-          <Text size="xl">Question: {currentQuestion}/5</Text>
+          <Text size="1.5rem">Question: {currentQuestion}/5</Text>
 
           <Button
             compact
-            size="xl"
+            size="1.5rem"
+            styles={{ root: { padding: "0.5rem" } }}
             onClick={() => {
               // TODO
             }}
@@ -64,7 +65,7 @@ function GameScreen() {
             Confirm
           </Button>
 
-          <Text size="xl">Score: {score}</Text>
+          <Text size="1.5rem">Score: {score}</Text>
         </Flex>
       </Paper>
     </Flex>
