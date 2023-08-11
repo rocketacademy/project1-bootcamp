4f5eaf869e4f9f583398f62aeb64868488c9f909
chengwaikoo <chengwai.koo@gmail.com>
2023-08-12 02:34:51 +0800

    refactor: remove  unnecessary Flex

--- src/components/ScoreOverlay.js
+++ src/components/ScoreOverlay.js
@@ -1,5 +1,5 @@
 import { useEffect } from "react";
-import { Overlay, Flex, Paper, Text } from "@mantine/core";
+import { Overlay, Paper, Text } from "@mantine/core";
 import { findScore, writeDistance } from "../utils";
 
 function ScoreOverlay({ distance, setTotalScore }) {
@@ -11,12 +11,10 @@ function ScoreOverlay({ distance, setTotalScore }) {
 
   return (
     <Overlay bg="rgba(0, 0, 0, 0)">
-      <Flex direction="column">
-        <Paper p="sm" radius="0" c="white" bg="rgba(0, 0, 0, 0.6)">
-          <Text size="md">Distance: {writeDistance(distance)}</Text>
-          <Text size="md">Score: {score}</Text>
-        </Paper>
-      </Flex>
+      <Paper p="sm" radius="0" c="white" bg="rgba(0, 0, 0, 0.6)">
+        <Text size="md">Distance: {writeDistance(distance)}</Text>
+        <Text size="md">Score: {score}</Text>
+      </Paper>
     </Overlay>
   );
 }
