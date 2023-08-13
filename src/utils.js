632110f15b44c19162fb91cbada0604782cb5aa7
chengwaikoo <chengwai.koo@gmail.com>
2023-08-13 17:54:11 +0800

    docs: add JSDocs for utils

--- src/utils.js
+++ src/utils.js
@@ -2,6 +2,11 @@ const SCORE_MAX = 100;
 const SCORE_BUFFER = 0.3;
 const SCORE_MAP_SIZE = 63;
 
+/**
+ * Shuffles array using Fisher-Yates algorithm
+ * @param {*[]} array array to be shuffled
+ * @returns {*[]} shuffled array
+ */
 const shuffle = (array) => {
   const shuffled = [...array];
   for (let i = shuffled.length - 1; i > 0; i--) {
@@ -11,6 +16,11 @@ const shuffle = (array) => {
   return shuffled;
 };
 
+/**
+ * Computes score using exponential decay
+ * @param {number} distance distance from correct location in kilometres
+ * @returns {number} score, where 0 <= score <= 100
+ */
 const findScore = (distance) => {
   return Math.round(
     SCORE_MAX *
@@ -18,6 +28,11 @@ const findScore = (distance) => {
   );
 };
 
+/**
+ * Round distance and add units for display
+ * @param {number} distance distance to be displayed
+ * @returns {string} representation of distance
+ */
 const writeDistance = (distance) => {
   if (distance < 1) {
     return (distance * 1000).toFixed(0) + " m";
