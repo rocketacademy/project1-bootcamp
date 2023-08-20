634dd6687bbb0b39da286e4a0a3b7b2e96b308c0
chengwaikoo <chengwai.koo@gmail.com>
2023-08-20 18:30:55 +0800

    feat: align question left

--- src/components/Question.js
+++ src/components/Question.js
@@ -4,7 +4,7 @@ function Question({ placeName }) {
   return (
     <Flex direction="column" align="flex-start">
       <Text size="md">Where's this MRT station ah?</Text>
-      <Text size="xl">
+      <Text size="xl" ta="left">
         <span id="place-name">{placeName}</span>
       </Text>
     </Flex>
