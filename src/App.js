8487ca928744509edebd446d77c7e1802f44193b
charlesleezhaoyi <charlesleezhaoyi@gmail.com>
2023-10-31 21:07:59 +0800

    Commit #1: Initialised Bootstrap and added NavBar

--- src/App.js
+++ src/App.js
@@ -1,17 +1,30 @@
 import React from "react";
 import logo from "./logo.png";
 import "./App.css";
+//Initialised bootstrap
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min";
 
 class App extends React.Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-        </header>
+      <div>
+        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+          <div className="container-fluid">
+            <a className="navbar-brand" href="/">
+              My GPA Calculator
+            </a>
+          </div>
+        </nav>
+
+        <div className="App">
+          <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <p>
+              Edit <code>src/App.js</code> and save to reload.
+            </p>
+          </header>
+        </div>
       </div>
     );
   }
