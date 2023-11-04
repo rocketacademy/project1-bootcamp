11a24aa3423b3bef43fd3da116405510a29b16f8
qiannnly <qianlinggg@gmail.com>
2023-11-04 11:57:59 +0800

    added localstorage to save username

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 
 const Home = () => {
@@ -15,6 +15,10 @@ const Home = () => {
     }
     navigate("/dashboard");
   };
+
+  useEffect(() => {
+    localStorage.setItem("user", JSON.stringify(username));
+  }, [username]);
 
   return (
     <>
