b00d5565f144173ed10f83a65dff60a031710489
qiannnly <qianlinggg@gmail.com>
2023-11-04 18:16:18 +0800

    added logic to hide and unhide form

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,11 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
+
 import { useNavigate } from "react-router-dom";
+import Timer from "../components/Timer";
+import Greetings from "./Greetings";
+
+import Form from "./Form";
 
 const Dashboard = () => {
+  const [show, setShow] = useState(false);
+  console.log(show);
   const navigate = useNavigate();
+
+  const getName = () => {
+    return JSON.parse(localStorage.getItem("user"));
+  };
+
   return (
     <>
-      <h1>Dashboard</h1>
+      <h2>{getName()}</h2>
+      <Timer />
+      <Greetings />
+
+      <h3>What would you like to do today?</h3>
+      {}
+      <button onClick={() => setShow(true)}>Add a Trip</button>
+
+      <h6>Ongoing trips</h6>
+      {!show ? (
+        <p>This space is currently empty! Add a trip to start.</p>
+      ) : (
+        <Form />
+      )}
+
       <button onClick={() => navigate(-1)}>Back</button>
     </>
   );
