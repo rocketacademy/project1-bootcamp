983140364b019264471fffae018db0592f90a51f
magiicloud <hongyun_rc@outlook.com>
2023-11-04 12:36:02 +0800

    added title component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import logo from "./logo.png";
 import "./App.css";
+import { Title } from "./components/title/title";
 
 class App extends React.Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
+          <Title />
           <img src={logo} className="App-logo" alt="logo" />
           <p>
             Edit <code>src/App.js</code> and save to reload.
---
+++ src/components/title/title.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export const Title = () => {
+  return (
+    <>
+      <h1>Pediatric Dose Calculator</h1>
+      <h2>Instructions</h2>
+      <p>Select the drug and enter patient’s weight</p>
+    </>
+  );
+};
