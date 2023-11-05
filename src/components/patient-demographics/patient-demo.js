2d9f97468df68b1c90698de43aaf9a3b180dcfbb
magiicloud <hongyun_rc@outlook.com>
2023-11-05 14:16:20 +0800

    added css to demographics

--- src/components/patient-demographics/patient-demo.js
+++ src/components/patient-demographics/patient-demo.js
@@ -6,8 +6,10 @@ export const Demographic = () => {
   return (
     <div className="demographic">
       <h2>Patient Demographics</h2>
-      <AgeInput />
-      <WeightInput />
+      <div className="demographic-input">
+        <AgeInput />
+        <WeightInput />
+      </div>
     </div>
   );
 };
