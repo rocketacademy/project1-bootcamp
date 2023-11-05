12a5bbac6216da9ef416071e4a1621aa2a2312c7
magiicloud <hongyun_rc@outlook.com>
2023-11-05 14:50:05 +0800

    added druglist in app.js and calculator empty function

--- src/App.js
+++ src/App.js
@@ -4,7 +4,32 @@ import { Title } from "./components/title/title";
 import { Demographic } from "./components/patient-demographics/patient-demo";
 import { Result } from "./components/resultsbox/result";
 
+const drugList = [
+  {
+    drugName: "paracetamol",
+    minDoseMgPerKg: 10,
+    maxDoseMgPerKg: 15,
+    freq: "QDS",
+    minAge: 1,
+  },
+  {
+    drugName: "ibuprofen",
+    minDoseMgPerKg: 5,
+    maxDoseMgPerKg: 10,
+    freq: "TDS",
+    minAge: 5,
+  },
+];
+
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {};
+  }
+
+  doseCalculator = () => {};
+
   render() {
     return (
       <div className="App">
