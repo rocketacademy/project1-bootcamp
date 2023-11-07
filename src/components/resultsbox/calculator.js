5efa96f772ee8be381c0746298d3948557ead004
magiicloud <hongyun_rc@outlook.com>
2023-11-07 22:15:34 +0800

    changed to grid table instead

--- src/components/resultsbox/calculator.js
+++ src/components/resultsbox/calculator.js
@@ -1,12 +1,6 @@
 import React from "react";
-import Table from "@mui/material/Table";
-import TableBody from "@mui/material/TableBody";
-import TableCell from "@mui/material/TableCell";
-import TableContainer from "@mui/material/TableContainer";
-import TableHead from "@mui/material/TableHead";
-import TableRow from "@mui/material/TableRow";
-import Paper from "@mui/material/Paper";
 import { DrugList } from "../druglist";
+import GridTable from "./gridtable";
 
 export const DoseCalculator = ({ selectedDrug, weightInput }) => {
   //reference selected drug to drugList
@@ -73,50 +67,7 @@ export const DoseCalculator = ({ selectedDrug, weightInput }) => {
         </tbody>
       </table> */}
 
-      <TableContainer component={Paper}>
-        <Table sx={{ minWidth: 650 }} size="small" aria-label="a dense table">
-          <TableHead>
-            <TableRow>
-              <TableCell>Drug Name</TableCell>
-              <TableCell align="right">Min Dose (mg)</TableCell>
-              <TableCell align="right">Max Dose (mg)</TableCell>
-              <TableCell align="right">Freq</TableCell>
-            </TableRow>
-          </TableHead>
-          <TableBody>
-            {dose.map((drug) => (
-              <TableRow
-                key={drug.drug}
-                // sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
-              >
-                <TableCell component="th" scope="row">
-                  {drug.drug.toUpperCase()}
-                </TableCell>
-                <TableCell align="right">
-                  {drug.freq === "BD"
-                    ? (drug.minDose / 2).toFixed(2)
-                    : drug.freq === "TDS"
-                    ? (drug.minDose / 3).toFixed(2)
-                    : drug.freq === "QDS"
-                    ? (drug.minDose / 4).toFixed(2)
-                    : drug.minDose + " /day"}
-                </TableCell>
-                <TableCell align="right">
-                  {" "}
-                  {drug.freq === "BD"
-                    ? (drug.maxDose / 2).toFixed(2)
-                    : drug.freq === "TDS"
-                    ? (drug.maxDose / 3).toFixed(2)
-                    : drug.freq === "QDS"
-                    ? (drug.maxDose / 4).toFixed(2)
-                    : drug.maxDose + " /day"}
-                </TableCell>
-                <TableCell align="right">{drug.freq}</TableCell>
-              </TableRow>
-            ))}
-          </TableBody>
-        </Table>
-      </TableContainer>
+      <GridTable dose={dose} />
     </>
   );
 };
---
+++ src/components/resultsbox/gridtable.js
@@ -0,0 +1,26 @@
+import React from "react";
+import { DataGrid } from "@mui/x-data-grid";
+
+const columns = [
+  { field: "drug", headerName: "Drug Name", width: 150 },
+  { field: "minDose", headerName: "Min Dose (mg)", width: 150 },
+  { field: "maxDose", headerName: "Max Dose (mg)", width: 150 },
+  { field: "freq", headerName: "Freq", width: 150 },
+];
+
+const GridTable = ({ dose }) => {
+  return (
+    <div>
+      <DataGrid
+        rows={dose}
+        columns={columns}
+        getRowId={(row) => row.drug}
+        pageSize={5}
+        autoHeight
+        checkboxSelection
+      />
+    </div>
+  );
+};
+
+export default GridTable;
