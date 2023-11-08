362b5c7929198a1d9ad46596f5130f4aa647088c
Mannushka <zaimar1910@gmail.com>
2023-11-08 23:42:09 +0800

    display itemsList and editItem functions added

--- src/Components/ItemsList.js
+++ src/Components/ItemsList.js
@@ -1,15 +1,18 @@
 import React from "react";
 import UserInput from "./UserInput";
-// import DeleteItemButton from "./DeleteItemButton";
+
 export default class ItemsList extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      itemsList: ["banana", "papaya", "mango"],
+      itemsList: [],
     };
   }
   addItem = (inputValue) => {
-    let newItemsList = [...this.state.itemsList, inputValue];
+    let newItem = { name: " ", key: " " };
+    newItem.name = inputValue;
+    newItem.key = Date.now();
+    let newItemsList = [...this.state.itemsList, newItem];
     this.setState({ itemsList: newItemsList });
   };
 
@@ -19,26 +22,55 @@ export default class ItemsList extends React.Component {
     itemsList.splice(index, 1);
     this.setState({ itemsList: itemsList });
   };
-  render() {
-    console.log(this.state.itemsList);
-    const newArray = this.state.itemsList.map((item, index) => {
+  updateItem = (name, key) => {
+    this.state.itemsList.map((item) => {
+      if (item.key === key) {
+        item.name = name;
+      }
+      return <span>{item.name}</span>;
+    });
+    this.setState({ itemsList: this.state.itemsList });
+  };
+
+  displayItemsList = () => {
+    const itemsList = this.state.itemsList.map((item, key) => {
       return (
-        <p key={index}>
-          <input type="checkbox" name="item" value={item}></input> {item}{" "}
-          <button type="button">Edit</button>
-          <button type="button" onClick={() => this.deleteItem(index)}>
-            Delete
-          </button>
-        </p>
+        <div key={item.key}>
+          <input type="checkbox" name="item" value={item}></input>
+          <span>
+            <input
+              type="text"
+              id={item.key}
+              value={item.name}
+              onChange={(event) =>
+                this.updateItem(event.target.value, item.key)
+              }
+            ></input>
+          </span>
+          {/* <img
+            id="edit_icon"
+            // onClick={()=> ()}
+            src="https://icons.veryicon.com/png/o/miscellaneous/linear-small-icon/edit-246.png"
+            alt="edit_icon"
+          /> */}
+          <img
+            id="delete_icon"
+            onClick={() => this.deleteItem(key)}
+            src="https://cdn-icons-png.flaticon.com/512/1214/1214428.png"
+            alt="delete_icon"
+          />
+        </div>
       );
     });
-
+    return itemsList;
+  };
+  render() {
     return (
       <div>
         <div>
           <UserInput addItem={this.addItem} />
         </div>
-        <div>{newArray}</div>
+        <div>{this.displayItemsList()}</div>
       </div>
     );
   }
@@ -46,3 +78,5 @@ export default class ItemsList extends React.Component {
 
 //We need to be able to add items to the list
 //Items are added in the UserInput component: when user inputs something into the input field and then clicks "add item" , the item should get added into the itemsArray
+//Each item can be deleted
+//Each item can be changed/edited.
