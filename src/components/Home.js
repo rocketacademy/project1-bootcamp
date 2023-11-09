77580c272e4183b23c0fdf2e917ce09be2c0c649
qiannnly <qianlinggg@gmail.com>
2023-11-09 14:14:32 +0800

    updated component with mui and localStorage

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,5 +1,22 @@
 import React, { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
+import {
+  Button,
+  TextField,
+  Typography,
+  Stack,
+  InputAdornment,
+} from "@mui/material";
+import { createTheme } from "@mui/material/styles";
+import { AccountCircle } from "@mui/icons-material";
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: "#00c853",
+    },
+  },
+});
 
 const Home = () => {
   const [username, setUsername] = useState("");
@@ -13,28 +30,54 @@ const Home = () => {
     if (username === "") {
       return;
     }
+    // store username in local storage
+    localStorage.setItem("user", JSON.stringify(username));
     navigate("/dashboard");
   };
 
   useEffect(() => {
-    localStorage.setItem("user", JSON.stringify(username));
-  }, [username]);
+    const userData = localStorage.getItem("user");
+    if (userData !== null) {
+      setUsername(JSON.parse(userData));
+    }
+  }, []);
 
   return (
     <>
-      {/*<h4>EQUALS</h4>*/}
-      <form>
-        <h6>Hey there!</h6>
-        <input
-          type="text"
-          value={username}
-          onChange={handleChange}
-          placeholder="Your name goes here"
-        />
-        <button type="button" onClick={handleClick}>
-          Get Started
-        </button>
-      </form>
+      <Stack spacing={4} direction="column">
+        <Typography
+          variant="h4"
+          sx={{ fontFamily: "Raleway", fontWeight: "bold" }}
+        >
+          Hey there!
+        </Typography>
+        <Stack>
+          <TextField
+            placeholder="Your name goes here"
+            value={username}
+            onChange={handleChange}
+            variant="outlined"
+            sx={{ fontStyle: "italic" }}
+            InputProps={{
+              startAdornment: (
+                <InputAdornment position="start">
+                  <AccountCircle />
+                </InputAdornment>
+              ),
+            }}
+          ></TextField>
+        </Stack>
+        <Stack>
+          <Button
+            variant="contained"
+            size="medium"
+            onClick={handleClick}
+            theme={theme}
+          >
+            Get Started
+          </Button>
+        </Stack>
+      </Stack>
     </>
   );
 };
