38c010f282eb4d8571ba222e9ddcbac01fedfbb4
Mannushka <zaimar1910@gmail.com>
2023-11-11 09:24:26 +0800

    minor changes

--- src/App.js
+++ src/App.js
@@ -2,27 +2,19 @@ import React from "react";
 import "./App.css";
 import ItemsList from "./Components/ItemsList";
 
-// import ItemsList from "./Components/ItemsList";
-
 class App extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {
-      userInput: "",
-      updatedUserInput: null,
-      itemsArray: [],
-      itemInCart: false,
-      itemHasBeenDeleted: false,
-    };
+    this.state = {};
   }
 
   render() {
     return (
       <div className="App">
-        <legend>
+        <div className="Header">
           <h1>Today's shopping list</h1>
-        </legend>
-        <div>
+        </div>
+        <div className="ItemsList">
           <ItemsList />
         </div>
       </div>
