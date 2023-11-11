35587351c9ad3ed23fe38774d16e7f1758afd650
ianthehamster <ianchow9898@gmail.com>
2023-11-11 17:06:34 +0800

    Correcting bugs in tasksInReview

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { DndProvider } from "react-dnd";
+import { HTML5Backend } from "react-dnd-html5-backend";
 import "./index.css";
 import App from "./App";
 
