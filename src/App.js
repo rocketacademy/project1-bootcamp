a4bf5ff505aa7c81dbd85654a4c69bf769c238a1
qiannnly <qianlinggg@gmail.com>
2023-11-12 19:46:16 +0800

    updated routes

--- src/App.js
+++ src/App.js
@@ -2,17 +2,26 @@ import React from "react";
 import { Routes, Route } from "react-router-dom";
 import Home from "./components/Home";
 import Dashboard from "./components/Dashboard";
+import AddTransactionsForm from "./components/AddTransactionsForm";
+import PastTrips from "./components/PastTrips";
 
 import "./App.css";
 
 const App = () => {
   return (
     <>
-      <div className="App">
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="dashboard" element={<Dashboard />} />
-        </Routes>
+      <div className="container">
+        <div className="form-wrapper ">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="dashboard" element={<Dashboard />} />
+            <Route
+              path="add-transactions-form"
+              element={<AddTransactionsForm />}
+            />
+            <Route path="past-trips" element={<PastTrips />} />
+          </Routes>
+        </div>
       </div>
     </>
   );
