e0e044b2bc211142597a2715a12dcc4e713a5d3d
Mannushka <zaimar1910@gmail.com>
2023-11-13 01:24:47 +0800

    added bootstrap container, changed regular form to bootstrap form

--- src/Components/Items.js
+++ src/Components/Items.js
@@ -1,39 +1,54 @@
 import React from "react";
 import { BsTrash3Fill } from "react-icons/bs";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Form from "react-bootstrap/Form";
+import Container from "react-bootstrap/Container";
 export default class Items extends React.Component {
   render() {
     const items = this.props.itemsList.map((item, key) => {
       return (
         <div key={item.key}>
-          <input
-            className="input"
-            type="checkbox"
-            id={item.key}
-            value={item.isChecked}
-            onChange={() => this.props.checkItem(item.key)}
-          ></input>
-          <span>
-            <input
-              className={item.isChecked ? "purchased-items" : "items-to-buy"}
-              type="text"
-              id={item.key}
-              value={item.name}
-              onChange={(event) =>
-                this.props.updateItem(event.target.value, item.key)
-              }
-            ></input>
-          </span>
-          <span>
-            <BsTrash3Fill
-              className="icons"
-              id="trashbin"
-              onClick={() => this.props.deleteItem(this.props.key)}
-            />
-          </span>
+          <Container>
+            <Form>
+              <Row className="align-items-center">
+                <Col xs={1}>
+                  <Form.Check
+                    className="input"
+                    type="checkbox"
+                    id={item.key}
+                    value={item.isChecked}
+                    onChange={() => this.props.checkItem(item.key)}
+                  />
+                </Col>
+                <Col xs={9}>
+                  <Form.Control
+                    className={
+                      item.isChecked ? "purchased-item-row" : "item-to-buy-row"
+                    }
+                    type="text"
+                    id={item.key}
+                    value={item.name}
+                    onChange={(event) =>
+                      this.props.updateItem(event.target.value, item.key)
+                    }
+                  />
+                </Col>
+                <Col xs={1} md={2}>
+                  <BsTrash3Fill
+                    className="icons"
+                    id="trashbin"
+                    onClick={() => this.props.deleteItem(key)}
+                  />
+                </Col>
+              </Row>
+            </Form>
+          </Container>
         </div>
       );
     });
-    console.log(this.props.itemsList);
+
     return <div>{items}</div>;
   }
 }
