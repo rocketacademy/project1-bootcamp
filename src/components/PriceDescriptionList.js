1aecc6e485b726705640d4b361f27b9db33db1c5
qiannnly <qianlinggg@gmail.com>
2023-11-12 19:32:35 +0800

    added mui styles

--- src/components/PriceDescriptionList.js
+++ src/components/PriceDescriptionList.js
@@ -1,18 +1,33 @@
 import React from "react";
+import { Button, Stack } from "@mui/material";
+import ClearIcon from "@mui/icons-material/Clear";
 
 const PriceDescriptionList = ({ details, deleteItems }) => {
-  console.log(details);
   return (
     <>
       <ul>
         {details.map((detail, index) => (
           <>
-            <li key={index.toString()}>
-              {detail.description} --- {detail.price} - {detail.payer}
-            </li>
-            <button type="button" onClick={() => deleteItems(index)}>
-              Delete
-            </button>
+            <Stack direction="row">
+              <li key={index.toString()}>
+                Item: {detail.description}
+                <br></br>
+                Price: ${detail.price}
+                <br></br>
+                Payer: {detail.payer}
+                <br></br>
+                <br></br>
+              </li>
+              <Button
+                onClick={() => deleteItems(index)}
+                color="secondary"
+                size="small"
+                variant="contained"
+                disableRipple
+              >
+                <ClearIcon />
+              </Button>
+            </Stack>
           </>
         ))}
       </ul>
