ce7aba419e3c7b06ec520865abb61803476a0753
patrickkok <pkok1996@gmail.com>
2023-11-14 21:47:07 +0800

    Add css to center icons, change class to className in icons

--- src/Components/Follow.js
+++ src/Components/Follow.js
@@ -18,18 +18,18 @@ class Follow extends React.Component {
             href="https://www.facebook.com/intimatesuppers/"
             className="follow-link"
           >
-            <i class="fa-brands fa-square-facebook"></i>
+            <i className="fa-brands fa-square-facebook"></i>
             <span>Facebook</span>
           </a>
           <a
             href="https://instagram.com/intimatesuppers?igshid=MXhkc25pYWw0OXkzZA=="
             className="follow-link"
           >
-            <i class="fa-brands fa-square-instagram"></i>
+            <i className="fa-brands fa-square-instagram"></i>
             <span>Instagram</span>
           </a>
           <a href="https://t.me/+51G0to4dOdM4YTBl" className="follow-link">
-            <i class="fa-brands fa-telegram"></i>
+            <i className="fa-brands fa-telegram"></i>
             Telegram
           </a>
         </div>
