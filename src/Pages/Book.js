c2f214b96abb3f6ee5a01f2afd937135e1a5605f
patrickkok <pkok1996@gmail.com>
2023-11-14 22:07:03 +0800

    Add background to book page

--- src/Pages/Book.js
+++ src/Pages/Book.js
@@ -2,6 +2,9 @@ import React from "react";
 import OurNav from "../Components/OurNav.js";
 import teleQR from "../images/teleQR.jpg";
 import teleBook from "../images/teleBook.jpg";
+import Follow from "../Components/Follow.js";
+
+import backgroundWide1 from "../images/backgroundWide1.jpg";
 
 import "./Book.css";
 
@@ -11,8 +14,8 @@ class Book extends React.Component {
       <div className="cont">
         <OurNav />
         <div
-          className="col-lg-12 book-container"
-          // style={{ backgroundImage: `url(${background1})` }}
+          className="col-lg-12 book-container book-background"
+          style={{ backgroundImage: `url(${backgroundWide1})` }}
         >
           <h1 className="book-header mb-2">
             Scan the code below or click to book with Celina!
@@ -26,7 +29,7 @@ class Book extends React.Component {
             />
           </a>
         </div>
-        <div className="col-lg-12">
+        <div className="col-lg-12 book-container">
           <h2 className="book-header">
             Scan the code below or click to stay updated with our intimate
             events
@@ -39,6 +42,9 @@ class Book extends React.Component {
               alt="QR code to join Telegram channel"
             />
           </a>
+        </div>
+        <div className="follow">
+          <Follow />
         </div>
       </div>
     );
