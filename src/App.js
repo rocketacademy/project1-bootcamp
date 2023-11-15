f2b730a613c3ff6f670e017be9e58912f18e522c
qiannnly <qianlinggg@gmail.com>
2023-11-15 21:28:59 +0800

    added inline styles to style header and footer

--- src/App.js
+++ src/App.js
@@ -11,6 +11,15 @@ const App = () => {
   return (
     <>
       <div className="container">
+        <div
+          style={{
+            backgroundColor: "#f48fb1",
+            width: "100vw",
+            height: "30vh",
+            borderRadius: "0px 0px 0px 150px",
+          }}
+        ></div>
+        <p className="hello"></p>
         <div className="form-wrapper ">
           <Routes>
             <Route path="/" element={<Home />} />
@@ -22,6 +31,14 @@ const App = () => {
             <Route path="past-trips" element={<PastTrips />} />
           </Routes>
         </div>
+        <div
+          style={{
+            backgroundColor: "#fce4ec",
+            width: "100vw",
+            height: "30vh",
+            borderRadius: "0px 150px 0px 0px",
+          }}
+        ></div>
       </div>
     </>
   );
