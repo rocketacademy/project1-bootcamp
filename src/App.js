64b617ea1ef2dd826bc645bf4ca5208932d97abd
qiannnly <qianlinggg@gmail.com>
2023-11-17 16:52:23 +0800

    added div tags for logo and renamed classes

--- src/App.js
+++ src/App.js
@@ -2,43 +2,28 @@ import React from "react";
 import { Routes, Route } from "react-router-dom";
 import Home from "./components/Home";
 import Dashboard from "./components/Dashboard";
-import AddTransactionsForm from "./components/AddTransactionsForm";
-import PastTrips from "./components/PastTrips";
 
 import "./App.css";
 
 const App = () => {
   return (
     <>
-      <div className="container">
-        <div
-          style={{
-            backgroundColor: "#f48fb1",
-            width: "100vw",
-            height: "30vh",
-            borderRadius: "0px 0px 0px 150px",
-          }}
-        ></div>
-        <p className="hello"></p>
-        <div className="form-wrapper ">
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="dashboard" element={<Dashboard />} />
-            <Route
-              path="add-transactions-form"
-              element={<AddTransactionsForm />}
-            />
-            <Route path="past-trips" element={<PastTrips />} />
-          </Routes>
+      <div className="app-container">
+        <div className="app-header">
+          <div className="app-logo-wrapper">
+            <div className="left-leaf"></div>
+            <div className="right-leaf"></div>
+          </div>
         </div>
-        <div
-          style={{
-            backgroundColor: "#fce4ec",
-            width: "100vw",
-            height: "30vh",
-            borderRadius: "0px 150px 0px 0px",
-          }}
-        ></div>
+
+        <div className="form-wrapper">
+          <div style={{ margin: "auto" }}>
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="dashboard" element={<Dashboard />} />
+            </Routes>
+          </div>
+        </div>
       </div>
     </>
   );
