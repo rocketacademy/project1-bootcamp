ea172f65692d8bd996d3b451d157338b9d86620b
ianthehamster <ianchow9898@gmail.com>
2023-11-17 21:52:48 +0800

    Fixed bug in index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { DndProvider } from "react-dnd";
-import { HTML5Backend } from "react-dnd-html5-backend";
+
 import "./index.css";
 import App from "./App";
 
