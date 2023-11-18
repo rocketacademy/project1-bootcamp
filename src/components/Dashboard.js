0d7f3de71c83835b03322672f28f7b987a235d38
qiannnly <qianlinggg@gmail.com>
2023-11-18 23:05:56 +0800

    added Box element and updated styles

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import Greetings from "./greetings/Greetings";
-import { Stack, Button } from "@mui/material";
+import { Stack, Button, Box } from "@mui/material";
 
 import Forms from "./forms/Forms";
 
@@ -10,35 +10,58 @@ const Dashboard = () => {
   const username = JSON.parse(localStorage.getItem("user"));
 
   // get tripname and companions details
-  const getTripData = JSON.parse(localStorage.getItem("tripDetails"));
+  const getActivityData = JSON.parse(localStorage.getItem("activityDetails"));
 
   return (
     <>
-      <Stack spacing={8}>
+      <Stack spacing={1}>
         <Greetings username={username} />
-        {!isTripFormShown && !getTripData ? (
-          <>
-            <p>You have no ongoing activity. Add a trip to begin.</p>
-            <div style={{ display: "flex", justifyContent: "center" }}>
-              <Button
-                onClick={() => {
-                  setIsTripFormShown(true);
-                }}
-                style={{
-                  backgroundColor: "#c6c09c",
-                  color: "black",
-                  fontWeight: "bold",
-                  width: "140px",
-                }}
-                disableRipple
-              >
-                Add a Trip
-              </Button>
-            </div>
-          </>
-        ) : (
-          <Forms />
-        )}
+        <Box
+          sx={{
+            height: "50vh",
+            margin: "0 auto",
+            borderRadius: "20px",
+            border: "2px solid orange",
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            padding: "20px",
+            bgcolor: "#dbcbbe",
+          }}
+        >
+          {!isTripFormShown && !getActivityData ? (
+            <>
+              <Stack spacing={5}>
+                <p
+                  style={{ fontSize: "18px", display: "block", margin: "auto" }}
+                >
+                  You have no ongoing activity. Add one to begin.
+                </p>
+                <div>
+                  <Button
+                    onClick={() => {
+                      setIsTripFormShown(true);
+                    }}
+                    style={{
+                      backgroundColor: "#ec407a",
+                      color: "white",
+                      fontWeight: "bold",
+                      width: "140px",
+
+                      display: "block",
+                      margin: "auto",
+                    }}
+                    disableRipple
+                  >
+                    Add Activity
+                  </Button>
+                </div>
+              </Stack>
+            </>
+          ) : (
+            <Forms />
+          )}
+        </Box>
       </Stack>
     </>
   );
