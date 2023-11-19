0dde7bd63e0de0b0053b3e6113d47bba2a012bdd
qiannnly <qianlinggg@gmail.com>
2023-11-19 17:13:54 +0800

    updated import location and some styles

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
-import Home from "./components/Home";
-import Dashboard from "./components/Dashboard";
+import Home from "./components/pages/Home";
+import Dashboard from "./components/pages/Dashboard";
 import Container from "@mui/material/Container";
 import Box from "@mui/material/Box";
 
@@ -10,50 +10,32 @@ import "./App.css";
 const App = () => {
   return (
     <>
-      <Container maxWidth="sm">
+      <Container sx={{ minWidth: "100vw", minHeight: "100vh" }}>
         <Box
           sx={{
-            bgcolor: "#e3dad5",
-            minHeight: "90vh",
-
-            margin: "30px",
-            borderRadius: "40px",
-            border: "2px solid black",
+            minHeight: "15vh",
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
           }}
         >
-          <Box
-            sx={{
-              height: "20vh",
-              margin: "20px",
-              borderRadius: "50px",
-              border: "2px solid red",
-              display: "flex",
-              justifyContent: "center",
-              alignItems: "center",
-              bgcolor: "#dbcbbe",
-            }}
-          >
-            <div className="left-leaf"></div>
-            <div className="right-leaf"></div>
-          </Box>
-          <Box
-            sx={{
-              height: "80vh",
-              margin: "20px",
-              borderRadius: "50px",
-              border: "2px solid green",
-              display: "flex",
-              justifyContent: "center",
-              alignItems: "center",
-              padding: "20px",
-              bgcolor: "#dbcbbe",
-            }}
-          >
-            <Routes>
-              <Route path="/" element={<Home />} />
-              <Route path="dashboard" element={<Dashboard />} />
-            </Routes>
-          </Box>
+          <div className="left-leaf"></div>
+          <div className="right-leaf"></div>
+        </Box>
+        <Box
+          sx={{
+            minHeight: "80vh",
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            padding: "30px",
+            bgcolor: "#daa06d",
+          }}
+        >
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="dashboard" element={<Dashboard />} />
+          </Routes>
         </Box>
       </Container>
     </>
